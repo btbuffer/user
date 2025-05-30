4d7f6db02dadcfa56da012f720d38d3c9e1c4ce2
btbuffer <jimohsikiru20@gmail.com>
2025-05-30 21:13:16 +0100

    update index.js with secret key

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+#!/usr/bin/env node
+
 const express = require("express");
+const mongoose = require("mongoose");
+
 const router = require("./routes/user.routes");
+require("dotenv").config();
 
 // Create an express application
 const app = express();
+const port = 8080;
 
 // Treat and parse sent data as application/json
 app.use(express.json());
 app.use(router);
 
-const port = 8080;
-app.listen(port, () => console.log(`App listens on port ${port}`));
+async function init() {
+  app.listen(port, () => console.log(`App listens on port ${port}`));
+  await mongoose.connect(process.env.DBPOINTER);
+}
+
+init().catch((err) => console.log("Error: ", err.message));
