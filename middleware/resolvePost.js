8a6cb30698e5bb1424b4fa3476be7a8e6f9125e9
btbuffer <jimohsikiru20@gmail.com>
2025-06-23 16:27:51 +0100

    Update: restructure both update and delete handlers with DRY principle

---
+++ middleware/resolvePost.js
@@ -0,0 +1,37 @@
+const { mongoose } = require("mongoose");
+const Post = require("../models/post.model");
+
+const resolvePostById = async (request, response, next) => {
+  const {
+    params: { postId },
+    user,
+  } = request;
+
+  // Check for valid Post ID and respond with
+  // 400 status code for invalid or not suitable ID
+  const isValid =
+    mongoose.Types.ObjectId.isValid(postId) &&
+    String(new mongoose.Types.ObjectId(postId)) === postId;
+  if (!isValid) return response.status(400).send({ msg: "Invalid post ID" });
+
+  // Check if a user is authenticated to perform this action.
+  // Otherwise, respond with 401 status code: unauthorized,
+  // i.e., client not authenticated.
+  if (!user) {
+    return response.status(401).send({ msg: "Authentication required." });
+  }
+
+  const foundPost = await Post.findById(postId);
+
+  // Check if the post exist, otherwise respond with 404 status
+  // code: resource is not found.
+  if (!foundPost) {
+    return response.status(404).send({ message: "Post not found" });
+  }
+
+  request.post = { postId, creatorId: foundPost.creatorId };
+
+  next();
+};
+
+module.exports = resolvePostById;
