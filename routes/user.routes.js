8015bb8d36f4bae4ee79a5de24bc9b9e14143ce6
btbuffer <jimohsikiru20@gmail.com>
2025-06-23 10:53:39 +0100

    Add: modify user routes to include middleware, route handlers

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,21 @@
-#!/usr/bin/env node
-
 const { Router } = require("express");
+const verifyUser = require("../middleware/auth");
+
 const {
   getUsers,
   createUser,
   updateUser,
   deleteUser,
+  loginUser,
+  homeTest,
 } = require("../controller/user.controller");
 const router = Router();
 
-router.post("/user", createUser);
-router.get("/user", getUsers);
-router.put("/user/:id", updateUser);
-router.delete("/user/:id", deleteUser);
+router.get("/user/cookies", homeTest);
+router.post("/api/users", createUser);
+router.post("/api/users/auth", loginUser);
+router.get("/api/users", verifyUser, getUsers);
+router.put("/api/users/:id", updateUser);
+router.delete("/api/users/:id", deleteUser);
 
 module.exports = router;
