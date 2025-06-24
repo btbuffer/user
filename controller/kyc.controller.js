6326d02906cb787882558b3463b1f97cb036ef87
btbuffer <jimohsikiru20@gmail.com>
2025-06-24 22:47:32 +0100

    Modify getKyc handler to include edge case conditions

--- controller/kyc.controller.js
+++ controller/kyc.controller.js
@@ -1,36 +1,22 @@
-const jwt = require("jsonwebtoken");
+const { mongoose } = require("mongoose");
 
 const Kyc = require("../models/kyc.model");
 const User = require("../models/user.model");
 
 const createKyc = async (request, response) => {
-  const {
-    cookies: { token },
-    body,
-  } = request;
+  const { user, body } = request;
 
-  let user = null;
-
-  try {
-    const decodeUser = jwt.verify(token, process.env.SECRET);
-    user = decodeUser;
-  } catch (err) {
-    user = { userId: null, error: err.message };
-  }
-
-  if (!user.userId) return response.status(400).send({ msg: "Unauthorized" });
-
+  // Check a user not previously created a KYC profile
   const kycExist = await Kyc.findOne({ user: user.userId });
   if (kycExist) return response.send({ msg: "KYC already being profiled." });
 
   const kycProfile = new Kyc({ user: user.userId, ...body });
   await kycProfile.save();
 
+  // Update a login user information with the KYC profile
+  // details.
   await User.findByIdAndUpdate(user.userId, { kyc: kycProfile.id });
 
-  // const decodeUser = jwt.verify();
-
-  // console.log("Creating KYC...");
   return response.send(kycProfile);
 };
 
@@ -38,7 +24,16 @@ const getKyc = async (request, response) => {
   const {
     params: { kycID },
   } = request;
-  const foundKyc = await Kyc.findById(kycID).populate("user");
+
+  const isValid =
+    mongoose.Types.ObjectId.isValid(kycID) &&
+    String(new mongoose.Types.ObjectId(kycID)) === kycID;
+
+  if (!isValid) return response.status(400).send({ msg: "Invalid kyc ID" });
+
+  const foundKyc = await Kyc.findById(kycID);
+  if (!foundKyc) return response.status(404).send({ msg: "Kyc not found" });
+  await foundKyc.populate("user");
 
   return response.send(foundKyc);
 };
