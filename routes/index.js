f39e5db9271d65063270f40c566b4f9205e5bb63
btbuffer <jimohsikiru20@gmail.com>
2025-06-24 20:48:15 +0100

    Add router for kyc domain

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const userRouters = require("./user.routes");
 const postRouters = require("./post.routes");
+const kycRouters = require("./kyc.routes");
 
 const router = Router();
 router.use(userRouters);
 router.use(postRouters);
+router.use(kycRouters);
 
 module.exports = router;
