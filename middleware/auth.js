92b713ea3078792c64e1127314069dc9b2aaf0a4
btbuffer <jimohsikiru20@gmail.com>
2025-06-24 22:53:51 +0100

    refactor auth implementation to check user login status

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,24 @@
 const jwt = require("jsonwebtoken");
 
-const verifyUser = (request, response, next) => {
+const authenticateUser = (request, response, next) => {
   const {
     cookies: { token },
   } = request;
 
   try {
-    const { userId } = jwt.verify(token, process.env.SECRET);
-    request.user = { userId };
+    const { userId, isAdmin } = jwt.verify(token, process.env.SECRET);
+    request.user = { userId, isAdmin };
   } catch (err) {
     request.user = undefined;
   }
 
+  // Check if a user is authenticated to perform this action.
+  // Otherwise, respond with 401 status code: unauthorized,
+  // i.e., client not authenticated.
+  if (!request.user)
+    return response.status(401).send({ msg: "Authentication required." });
+
   next();
 };
 
-module.exports = verifyUser;
+module.exports = authenticateUser;
