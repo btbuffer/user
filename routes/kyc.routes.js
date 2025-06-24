a7cd54dea3a66ee3a68eee292bb067e4d95fac76
btbuffer <jimohsikiru20@gmail.com>
2025-06-24 22:57:01 +0100

    Change import value name from verifyUser to authenticateUser'

--- routes/kyc.routes.js
+++ routes/kyc.routes.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
 
+const authenticateUser = require("../middleware/auth");
 const { createKyc, getKyc } = require("../controller/kyc.controller");
 
 const router = Router();
-router.post("/api/kycs", createKyc);
-router.get("/api/kycs/:kycID", getKyc);
+router.post("/api/kycs", authenticateUser, createKyc);
+router.get("/api/kycs/:kycID", authenticateUser, getKyc);
 
 module.exports = router;
