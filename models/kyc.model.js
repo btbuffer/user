46cdbad7650f9d34536ca707e01081110bec8d35
btbuffer <jimohsikiru20@gmail.com>
2025-06-27 20:42:30 +0100

    Update Kyc model with createdAt, UpdatedAt timestamps

--- models/kyc.model.js
+++ models/kyc.model.js
@@ -1,29 +1,32 @@
 const { mongoose } = require("mongoose");
 
-const KycSchema = new mongoose.Schema({
-  displayPix: {
-    type: String,
-    required: true,
+const KycSchema = new mongoose.Schema(
+  {
+    displayPix: {
+      type: String,
+      required: true,
+    },
+    docType: {
+      type: String,
+      required: true,
+    },
+    frontPix: {
+      type: String,
+      required: true,
+    },
+    backPix: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
   },
-  docType: {
-    type: String,
-    required: true,
-  },
-  frontPix: {
-    type: String,
-    required: true,
-  },
-  backPix: {
-    type: String,
-    required: true,
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Kyc = mongoose.model("Kyc", KycSchema);
 
