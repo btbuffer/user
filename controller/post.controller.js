8213f2d4bdcb28dcd8d39b13b34eeac0986abc26
btbuffer <jimohsikiru20@gmail.com>
2025-06-27 20:32:03 +0100

    Update code to include fetch-all-post

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,62 +1,59 @@
 const { mongoose } = require("mongoose");
 const Post = require("../models/post.model");
+const User = require("../models/user.model");
 
 // Create a post
 const createPost = async (request, response) => {
-  if (!request.user) {
-    return response
-      .status(401)
-      .send({ msg: "Please, login to create a post." });
-  }
-
   const {
     user: { userId },
     body,
   } = request;
 
-  const post = new Post({ ...body, creatorId: userId });
+  const post = new Post({ ...body, creator: userId });
   await post.save();
+  // First Method:
+  // const user = await User.findById(userId);
+  // console.log("USER: ", user);
+  // const posts = user.posts.push(post.id);
+  // console.log("POSTS: ", posts);
+  // await User.findByIdAndUpdate(userId, { posts: user });
+
+  // Second Method:
+  await User.findByIdAndUpdate(userId, { $push: { posts: post.id } });
   return response.json({ postId: post.id });
+};
+
+// Fetch all post
+const fetchAllPost = async (request, response) => {
+  const allPosts = await Post.find();
+  return response.send({ posts: allPosts });
 };
 
 // Retrieve a post
 const fetchPost = async (request, response) => {
-  if (!request.user) {
-    // is this condition necessary for fetchPost: || request.user.userId !== foundPost.creatorId
-    return response.status(401).send({ msg: "Unauthorized! Please, login" });
-  }
-
   const {
-    params: { postId },
+    post: { foundPost },
   } = request;
-
-  const isValid =
-    mongoose.Types.ObjectId.isValid(postId) &&
-    postId === String(new mongoose.Types.ObjectId(postId));
-
-  if (!isValid) {
-    return response.status(400).send({ msg: "Invalid post ID" });
-  }
-
-  const foundPost = await Post.findById(postId);
-
-  if (!foundPost) return response.status(404).send({ msg: "Post not found" });
 
   return response.status(200).send(foundPost);
 };
 
 // Update a post
 const updatePost = async (request, response) => {
-  const { user, post, body } = request;
+  const {
+    user,
+    post: { postId, foundPost },
+    body,
+  } = request;
   // Allow Post update only if the post's author is the one
   // making the changes. Otherwise, respond with 403 status
   // code: performed action is forbidden.
-  if (user.userId !== post.creatorId.toString())
+  if (user.userId !== foundPost.creator.toString())
     return response
       .status(403)
       .send({ msg: "You do not have permission to perform this action." });
 
-  const updatedPost = await Post.findByIdAndUpdate(post.postId, body, {
+  const updatedPost = await Post.findByIdAndUpdate(postId, body, {
     new: true,
   });
 
@@ -65,7 +62,10 @@ const updatePost = async (request, response) => {
 
 // Delete a post.
 const deletePost = async (request, response) => {
-  const { post, user } = request;
+  const {
+    post: { postId, foundPost },
+    user,
+  } = request;
 
   // Don't allow post deletion if a user is not the author
   // and/or not the admin. And respond with 403 status
@@ -75,13 +75,19 @@ const deletePost = async (request, response) => {
   // notOwner AND notAdmin === true && true = Do not have permission
   // notOwner AND isAdmin === true && false = Have permission
   // isOwner AND isAdmin === false && false = Havve permission
-  if (user.userId !== post.creatorId.toString() && !user.isAdmin)
+  if (user.userId !== foundPost.creator.toString() && !user.isAdmin)
     return response
       .status(403)
       .send({ msg: "You do not have permission to perform this action." });
 
-  await Post.findByIdAndDelete(post.postId);
+  await Post.findByIdAndDelete(postId);
   return response.status(200).send({ msg: "post successfully deleted!" });
 };
 
-module.exports = { createPost, fetchPost, updatePost, deletePost };
+module.exports = {
+  createPost,
+  fetchAllPost,
+  fetchPost,
+  updatePost,
+  deletePost,
+};
